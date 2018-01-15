a3187e9429b150764b6271fba348270c253091d6
RongfengCui <rongfengcui123@gmail.com>
2018-01-15 09:59:09 -0400

    create

---
+++ src/main/java/com/cui/csci11011/App.java
@@ -0,0 +1,13 @@
+package com.cui.csci11011;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
